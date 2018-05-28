7379bb923fc7d507518935ade172e301022d0d7d
AndrWeisR <34931916+AndrWeisR@users.noreply.github.com>
2018-05-29 08:31:14 +0930

    Update DownloadManagerPlugin.java

--- src/com/github/emilbayes/downloadmanager/DownloadManagerPlugin.java
+++ src/com/github/emilbayes/downloadmanager/DownloadManagerPlugin.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.pm.LauncherApps;
 import android.database.Cursor;
 import android.net.Uri;
+import android.util.Log;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
@@ -33,6 +34,7 @@ public class DownloadManagerPlugin extends CordovaPlugin {
     }
 
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+Log.i("action = "+action);
       if (action.equals("enqueue")) return enqueue(args.getJSONObject(0), callbackContext);
       if (action.equals("query")) return query(args.getJSONObject(0), callbackContext);
       if (action.equals("remove")) return remove(args, callbackContext);
@@ -74,6 +76,8 @@ public class DownloadManagerPlugin extends CordovaPlugin {
     }
 
     protected boolean addCompletedDownload(JSONObject req, CallbackContext callbackContext) throws JSONException {
+Log.i("In addCompletedDownload");
+Log.i("title = " + req.optString("title"));
 	    
         long id = downloadManager.addCompletedDownload(
             req.optString("title"),
